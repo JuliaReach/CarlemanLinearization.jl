1ea51714323af2846b7e584faf5414d1fe9af460
schillic <git@christianschilling.net>
2021-11-25 22:08:20 +0100

    revise scripts

--- docs/make.jl
+++ docs/make.jl
@@ -1,15 +1,11 @@
-ENV["GKSwstype"] = "100"  # set 'GR environment' to 'no output' (for Travis CI)
 using Documenter, CarlemanLinearization
 using DynamicPolynomials
 
 DocMeta.setdocmeta!(CarlemanLinearization, :DocTestSetup,
                    :(using CarlemanLinearization); recursive=true)
 
-# generate Literate documentation
-# include("generate.jl")
-
 makedocs(
-    format = Documenter.HTML(prettyurls = haskey(ENV, "GITHUB_ACTIONS"),  # disable for local builds
+    format = Documenter.HTML(prettyurls = haskey(ENV, "GITHUB_ACTIONS"),
                              collapselevel = 1),
     sitename = "CarlemanLinearization.jl",
     doctest = false,
@@ -23,8 +19,6 @@ makedocs(
     ]
 )
 
-# Deploy built documentation from Travis.
 deploydocs(
-    repo = "github.com/JuliaReach/CarlemanLinearization.jl.git",
-    push_preview = true,
+    repo = "github.com/JuliaReach/CarlemanLinearization.jl.git"
 )
