46f98ae0204f90d40a93163275889f87e4aa04e4
Marcelo Forets <mforets@gmail.com>
2022-01-14 15:04:34 -0300

    some fixes

--- src/init_MultivariatePolynomials.jl
+++ src/init_MultivariatePolynomials.jl
@@ -1,6 +1,6 @@
 eval(quote
     using .MultivariatePolynomials: AbstractVariable, AbstractMonomialLike,
-                                    exponents, variables, powers
+                                    exponents, variables, powers, monomials, coefficient
 end)
 
 eval(load_kron_multivariate())
