8eb0579b60095fdb5b0240baa8bad22ef68b541a
schillic <git@christianschilling.net>
2023-04-09 09:49:16 +0200

    load IntervalArithmetic directly instead of through LazySets

--- src/init.jl
+++ src/init.jl
@@ -6,6 +6,8 @@ using MultivariatePolynomials: AbstractVariable, AbstractMonomialLike,
 function __init__()
     @require LazySets = "b4f0291d-fe17-52bc-9479-3d1a343d9043" begin
         using .LazySets: Hyperrectangle, dim, low, high
-        using .LazySets.IntervalArithmetic: interval
+    end
+    @require IntervalArithmetic = "d1acc4aa-44c8-5952-acd4-ba5d80a2a253" begin
+        using .IntervalArithmetic: interval
     end
 end
