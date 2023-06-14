05646e1174637a0add81cf55413e010bf628d114
schillic <git@christianschilling.net>
2023-06-14 06:07:43 +0200

    format docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -2,23 +2,17 @@ using Documenter, CarlemanLinearization
 using DynamicPolynomials
 
 DocMeta.setdocmeta!(CarlemanLinearization, :DocTestSetup,
-                   :(using CarlemanLinearization); recursive=true)
+                    :(using CarlemanLinearization); recursive=true)
 
-makedocs(
-    sitename = "CarlemanLinearization.jl",
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true",
-                             assets = ["assets/aligned.css"]),
-    strict = true,
-    pages = [
-        "Home" => "index.md",
-        "API Reference" => Any["Linearization" => "lib/linearization.md",
-                               "Error bounds" => "lib/errors.md"],
-        "References" => "references.md",
-        "About" => "about.md"
-    ]
-)
+makedocs(; sitename="CarlemanLinearization.jl",
+         format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true",
+                                assets=["assets/aligned.css"]),
+         strict=true,
+         pages=["Home" => "index.md",
+                "API Reference" => Any["Linearization" => "lib/linearization.md",
+                                       "Error bounds" => "lib/errors.md"],
+                "References" => "references.md",
+                "About" => "about.md"])
 
-deploydocs(
-    repo = "github.com/JuliaReach/CarlemanLinearization.jl.git",
-    push_preview = true
-)
+deploydocs(; repo="github.com/JuliaReach/CarlemanLinearization.jl.git",
+           push_preview=true)
