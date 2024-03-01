586e76c22d8bad5ae88b7288e9bdf9127c719ae6
schillic <git@christianschilling.net>
2024-03-01 15:29:21 +0100

    refactor and remove dead code

--- src/error_bounds.jl
+++ src/error_bounds.jl
@@ -67,45 +67,34 @@ end
 # These bounds use the spectral norm (p = 2).
 
 # compute eigenvalues and sort them by increasing real part
-function _error_bound_specabs_Re_λ₁(F₁; check=true)
+function _error_bound_specabs_Re_λ₁(F₁)
     λ = eigvals(F₁; sortby=real)
-    λ₁ = last(λ)
-    Re_λ₁ = real(λ₁)
-    if check && Re_λ₁ > 0
-        throw(ArgumentError("expected Re(λ₁) ≤ 0, got $Re_λ₁"))
-    end
-    return Re_λ₁
-end
-
-# see Equation (2.2) in [2]
-function _error_bound_specabs_R(x₀, F₂, Re_λ₁)
-    nx₀ = norm(x₀, 2)
-    nF₂ = opnorm(F₂, 2)
-    R = nx₀ * nF₂ / abs(Re_λ₁)
-    return R
+    return real(last(λ))
 end
 
 # see Corollary 1 in [2]
 function error_bound_specabs(x₀, F₁, F₂; N, check=true)
-    Re_λ₁ = _error_bound_specabs_Re_λ₁(F₁; check=check)
-    R = _error_bound_specabs_R(x₀, F₂, Re_λ₁)
+    Re_λ₁ = _error_bound_specabs_Re_λ₁(F₁)
+    if check && Re_λ₁ >= 0
+        throw(ArgumentError("expected Re(λ₁) < 0, got $Re_λ₁"))
+    end
+
+    # Equation (2.2)
+    nx₀ = norm(x₀, 2)
+    nF₂ = opnorm(F₂, 2)
+    R = nx₀ * nF₂ / -Re_λ₁
     if check && R >= 1
         throw(ArgumentError("expected R < 1, got R = $R; try scaling the ODE"))
     end
 
-    nx₀ = norm(x₀, 2)
-    if iszero(Re_λ₁)
-        nF₂ = opnorm(F₂, 2)
-        ε = t -> nx₀ * (nx₀ * nF₂ * t)^N
-    else
-        ε = t -> nx₀ * R^N * (1 - exp(Re_λ₁ * t))^N  # Equation (4.29)
-    end
+    # Equation (4.29)
+    ε = t -> nx₀ * R^N * (1 - exp(Re_λ₁ * t))^N
     return ε
 end
 
 # see Corollary 1 in [2]
-function convergence_radius_specabs(x₀, F₁, F₂; check=true)
-    Re_λ₁ = _error_bound_specabs_Re_λ₁(F₁; check=check)
+function convergence_radius_specabs(x₀, F₁, F₂)
+    Re_λ₁ = _error_bound_specabs_Re_λ₁(F₁)
 
     if Re_λ₁ < 0
         T = Inf
