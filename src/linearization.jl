8b02b5f16a9b9cc7ad25e923178394ac9ac2c7b8
schillic <git@christianschilling.net>
2024-12-08 11:28:11 +0100

    use package extensions

--- src/linearization.jl
+++ src/linearization.jl
@@ -98,22 +98,14 @@ function build_matrix_compressed(F₁, F₂, N)
     return result
 end
 
-"""
-    lift_vector(X0, N)
-
-Return a vector of monomials in `X0` (hyperrectangle) of degree at most `N`.
-"""
 function lift_vector(X0, N)
-    require(@__MODULE__, :LazySets; fun_name="lift_vector")
-
-    monoms = generate_monomials(dim(X0), N)
-    nonfirst_monoms = Iterators.peel(monoms)[2]
-    result = []
-    intervals = [interval(low(X0, i), high(X0, i)) for i in 1:dim(X0)]
-    for m in nonfirst_monoms
-        push!(result, prod(intervals .^ m))
+    mod = isdefined(Base, :get_extension) ?
+          Base.get_extension(@__MODULE__, :LazySetsExt) : @__MODULE__
+    require(mod, :LazySets; fun_name="lift_vector")
+    if !isdefined(Base, :get_extension)
+        mod = LazySetsExt
     end
-    return Hyperrectangle(; low=[i.lo for i in result], high=[i.hi for i in result])
+    return mod._lift_vector(X0, N)
 end
 
 """
