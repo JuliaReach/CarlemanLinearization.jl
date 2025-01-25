486cbafe7d6296d9a0c74eca57a90e83523bb218
schillic <git@christianschilling.net>
2025-01-25 15:50:01 +0100

    use DocumenterCitations for bibliography

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,20 @@
-using Documenter, CarlemanLinearization
+using Documenter, CarlemanLinearization, DocumenterCitations
 using DynamicPolynomials
 
 DocMeta.setdocmeta!(CarlemanLinearization, :DocTestSetup,
                     :(using CarlemanLinearization); recursive=true)
 
+bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"); style=:alpha)
+
 makedocs(; sitename="CarlemanLinearization.jl",
          format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true",
-                                assets=["assets/aligned.css"]),
+                                assets=["assets/aligned.css", "assets/citations.css"]),
          pagesonly=true,
+         plugins=[bib],
          pages=["Home" => "index.md",
                 "API Reference" => Any["Linearization" => "lib/linearization.md",
                                        "Error bounds" => "lib/errors.md"],
-                "References" => "references.md",
+                "Bibliography" => "bibliography.md",
                 "About" => "about.md"])
 
 deploydocs(; repo="github.com/JuliaReach/CarlemanLinearization.jl.git",
